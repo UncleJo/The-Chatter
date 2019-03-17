748d5c9bb534846c93fbfa348e68727f0d02398a
Sanxchep Sharma <sanxchep@gmail.com>
2019-03-17 23:07:27 +0530

    assets

---
+++ include/menu.php
@@ -0,0 +1,107 @@
+<!-- Header -->
+					<header id="header">
+						<h1><a href="index.html"><b>The Chatter</b></a></h1>
+						<nav class="links">
+							
+						</nav>
+						<nav class="main">
+							<ul>
+								<!--<li class="search">
+									<a class="fa-search" href="#search">Search</a>
+									<form id="search" method="get" action="#">
+										<input type="text" name="query" placeholder="Search" />
+									</form>
+								</li>-->
+								<li class="menu">
+									<a class="fa-bars" href="#menu">Menu</a>
+								</li>
+							</ul>
+						</nav>
+					</header>
+
+				<!-- Menu -->
+					<section id="menu">
+
+						<!-- Search 
+							<section>
+								<form class="search" method="get" action="#">
+									<input type="text" name="query" placeholder="Search" />
+								</form>
+							</section>
+
+						<!-- Links -->
+							<section>
+								<ul class="links">
+									<li>
+										<a href="https://sanxchep.info/chatter_theme/">
+											<h3>Home</h3>
+											
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<h3>Events</h3>
+											
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<h3>Kaalrav</h3>
+											
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<h3>Clubs</h3>
+											
+										</a>
+									</li>
+									<li>
+										<a href="creatives/">
+											<h3>Creative Section</h3>
+											
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<h3>Peek in the Corner</h3>
+											<a href="#"><p>Hostel</p></a>
+											<a href="#"><p>Mess</p></a>
+											<a href="#"><p>Marena</p></a>
+											<a href="#"><p>Amphitheatre</p></a>
+											<a href="#"><p>Library</p></a>
+										</a>
+									</li>
+									<li>
+										<a href="about.html">
+											<h3>About Us</h3>
+											
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<h3>Contact Us</h3>
+											
+										</a>
+									</li>
+								</ul>
+							</section><br><br>
+							<section>
+								<ul class="links">
+									<li>
+										<a href="releases.html">
+											<h3>V 1.2.1 Pre-alpha</h3>
+											<p>Click here for release notes</p>
+										</a>
+									</li>
+								</ul>
+							</section>
+
+						<!-- Actions 
+							<section>
+								<ul class="actions stacked">
+									<li><a href="#" class="button large fit">Log In</a></li>
+								</ul>
+							</section>-->
+
+					</section>
